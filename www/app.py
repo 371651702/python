da7845da530778d188a74f3e3350e911d1070484
dongliang.wang <dongliang.wang@sumscope.com>
2018-11-26 12:31:29 +0800

    commit basic infomation

--- www/app.py
+++ www/app.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio
+import asyncio, os, json, time
+from datetime import datetime
 
 from aiohttp import web
 
@@ -8,10 +18,10 @@ def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
-    srv = yield from loop.create_server(app._make_handler(),'127.0.0.1',9000)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
